6d7369ecf27efad51aed471d7e357ef575615836
Lasse Rolstad <lasrol88@gmail.com>
2017-06-01 15:20:15 +0200

    Fixed minor refrence issues and removed old test

--- src/DiscordRest/Services/Implementations/UserService.cs
+++ src/DiscordRest/Services/Implementations/UserService.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
-using DiscordRest.Attributes;
 using DiscordRest.Data;
 using DiscordRest.Models;
 
--- test/DiscordRest.Tests/TypeExtensionsTests.cs
+++ test/DiscordRest.Tests/TypeExtensionsTests.cs
@@ -1,26 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using DiscordRest.Attributes;
-using DiscordRest.Extensions;
-using DiscordRest.Tests.Fake;
-using Shouldly;
-using Xunit;
-
-namespace DiscordRest.Tests
+﻿namespace DiscordRest.Tests
 {
     public class TypeExtensionsTests
     {
-        [Fact]
-        public void CanReadAttributeOnType()
-        {
-            //Arrange
-
-            //Act
-            var result = typeof(TestClassWithAttribute).GetAttribute<EndpointAttribute>();
-
-            //Assert
-            result.Url.ToString().ShouldBe("/some/url");
-        }
     }
 }
