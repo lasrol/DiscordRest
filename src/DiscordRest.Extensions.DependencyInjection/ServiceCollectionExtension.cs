ab588668998f9d80a94d0fd50dc0208b472f2f56
Lasse Rolstad <lasrol88@gmail.com>
2017-06-02 15:18:02 +0200

    Added extension to register with default core DI

---
+++ src/DiscordRest.Extensions.DependencyInjection/ServiceCollectionExtension.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using DiscordRest.Configuration;
+using DiscordRest.Utility;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace DiscordRest.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddDiscordRest<TStore, TCurrentUser>(this IServiceCollection container, Action<HttpConnectionOptions> options) 
+            where TStore : class, ITokenStore 
+            where TCurrentUser : class, ICurrentUserContext
+        {
+            var connectionOptions = new HttpConnectionOptions();
+            options.Invoke(connectionOptions);
+
+            container.TryAddSingleton(connectionOptions);
+            container.TryAddSingleton<ILogger>(NullLogger.Instance);
+
+            container.TryAddScoped<IHttpConnectionBuilder, HttpConnectionBuilder>();
+            container.TryAddScoped<IDiscordHttpClient, DiscordHttpClient>();
+
+
+            //Required user implementations
+            container.AddTransient<ITokenStore, TStore>();
+            container.AddTransient<ICurrentUserContext, TCurrentUser>();
+
+            //Dynamicly registert services
+            var discordRest = typeof(IDiscordServices).GetTypeInfo().Assembly;
+            foreach (var type in discordRest.ExportedTypes.Where(t => t.Namespace == "DiscordRest.Services"))
+            {
+                var implementation = discordRest.GetType(type.Name.Substring(1, type.Name.Length));
+                container.AddScoped(type, implementation);
+            }
+
+            return container;
+        }
+    }
+}
